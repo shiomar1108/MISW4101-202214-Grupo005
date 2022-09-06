8a7b6b7bce35f133e54b3fe3937bccbbcb7b1725
Andres Soler <andres.soler@talent.com>
2022-09-06 10:25:50 -0500

    se agrego dar_autos, dar_auto, crear_auto, eliminar_auto en logica_mock

--- src/vista/Vista_lista_autos.py
+++ src/vista/Vista_lista_autos.py
@@ -131,7 +131,7 @@ class Vista_lista_autos(QWidget):
                 # dic_auto = dic_auto.__dict__
                 numero_fila=numero_fila+1
 
-                etiqueta_nombre=QLabel(dic_auto['Placa'])
+                etiqueta_nombre=QLabel(dic_auto['placa'])
                 etiqueta_nombre.setWordWrap(True)
                 self.distribuidor_tabla_autos.addWidget(etiqueta_nombre,numero_fila,0)
 
@@ -161,11 +161,11 @@ class Vista_lista_autos(QWidget):
                 btn_eliminar.setToolTip("Borrar")
                 btn_eliminar.setFixedSize(40,40)
                 btn_eliminar.setIcon(QIcon("src/recursos/005-delete.png"))
-                # btn_eliminar.clicked.connect(partial(self.eliminar_auto,dic_auto['id']) )
-                btn_eliminar.clicked.connect(partial(self.eliminar_auto,numero_fila -1) )
+                btn_eliminar.clicked.connect(partial(self.eliminar_auto,dic_auto['id']) )
+                # btn_eliminar.clicked.connect(partial(self.eliminar_auto,numero_fila -1) )
                 self.distribuidor_tabla_autos.addWidget(btn_eliminar,numero_fila,4,Qt.AlignCenter)
 
-                if dic_auto['Vendido']:
+                if dic_auto['vendido']:
                     btn_ver_actividad.setDisabled(True)
                     btn_editar.setDisabled(True)
                     btn_terminar.setDisabled(True)
